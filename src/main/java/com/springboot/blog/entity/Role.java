4a06a5b5725abad60af788837431c0a028b520eb
Bohdan-Dubravin <dubravin111@gmail.com>
2023-09-06 08:21:51 +0300

    add user and role tables for authorization

---
+++ src/main/java/com/springboot/blog/entity/Role.java
@@ -0,0 +1,26 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  @Column(nullable = false, unique = true)
+  private String name;
+
+
+}
